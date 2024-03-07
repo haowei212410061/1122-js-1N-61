5462f27b60f08ec4394feca7566fc9ca8ba47899
haowei0218 <haoweizhou891@gmail.com>
2024-03-07 22:06:12 +0900

    fix p2 issue

--- demo/w03_basics_61/p2_61/p2_61.js
+++ demo/w03_basics_61/p2_61/p2_61.js
@@ -4,22 +4,30 @@ const result = document.querySelector('#input-number')
 const inputBox = document.querySelector('#current-calculation')
 const EndResult = document.querySelector('#current-calculation-1')
 
+const defaultResult = 0;
 
+let currentResult = defaultResult;
 
 function getUserInput() {
     return Number(result.value).toFixed(2); //get user input number
 }
+
+function outputResult(result,text){
+    inputBox.textContent = text;
+    EndResult.textContent = result;
+
+}
 function ChangeToC() {
     const FinishResult = parseFloat(getUserInput() * 9.0 / 5 - 32).toFixed(2);
-    inputBox.textContent = FinishResult;
-    EndResult.textContent = `${getUserInput()}C =  ${FinishResult}F`
+    const Number = `${getUserInput()}F =  ${FinishResult}C`
+    outputResult(Number,FinishResult)
 
 }
 
 function ChangeToF() {
     const FinishResult = parseFloat(getUserInput() * 5.0 / 9 + 32).toFixed(2)
-    inputBox.textContent = FinishResult;
-    EndResult.textContent = `${getUserInput()}C =  ${FinishResult}F`
+    const Number = `${getUserInput()}C =  ${FinishResult}F`
+    outputResult(Number,FinishResult)
 }
 
 
