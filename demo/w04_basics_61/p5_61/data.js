593b8253070f96d7ad0d0dcded6a03c80a6edb5b
haowei0218 <haoweizhou891@gmail.com>
2024-03-14 21:16:54 +0900

    P5_61 -- Display 5 tours from data_xx.js (json array)

--- demo/w04_basics_61/p5_61/data.js
+++ demo/w04_basics_61/p5_61/data.js
@@ -10,15 +10,32 @@ export const tours = [
         id: "2",
         name: 'Best of Ireland in 14 Days Tour',
         info: "'Rick Steves Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites,charming towns, music-filled pubs, and seaside getaways —including Kinsale, the D...'",
-        local_img: './tour_theme/img/ireland.jpg',
+        local_img: './tour_theme/img/ireland.jpeg',
         price: 3895
     },
     {
         id: "3",
-        name: 'Best of Salzburg &amp; Vienna in 8 Days Tour',
-        info: "'Rick Steves Best of Ireland tour kicks off with the best of Dublin, followed by Ireland's must-see historical sites,charming towns, music-filled pubs, and seaside getaways —including Kinsale, the D...'",
-        local_img: './tour_theme/img/ireland.jpg',
-        price: 3895
-    }
+        name: 'Best of Salzburg & Vienna in 8 Days Tour',
+        info: "Let's go where classical music, towering castles, and the-hills-are-alive scenery welcome you to the gemütlichkeit ofBavaria and opulence of Austria's Golden Age. Your Rick Stevesguide will bring th... and seaside getaways —including Kinsale, the D...",
+        local_img: './tour_theme/img/vienna.jpeg',
+        price: 2695
+    },
+    {
 
-]      
+        id: "4",
+        name: 'Best of Rome in 7 Days Tour',
+        info: "Our Rome tour serves up Europe's most intoxicating brew of dazzling art, earth-shaking history, and city life with style. On this Rome vacation, your tour guide will resurrect the grandeur of ancient ...",
+        local_img: './tour_theme/img/vienna.jpeg',
+        price: 2695
+
+    },
+    {
+
+        id: "5",
+        name: 'Best of Poland in 10 Days Tour',
+        info: " Starting in the colorful port city of Gdańsk, you'll escape the crowds and embrace the understated elegance of ready-for-prime-time Poland for 10 days. With an expert Rick Steves guide at your side, y...",
+        local_img: './tour_theme/img/poland.jpeg',
+        price: 2595
+
+    }
+];
