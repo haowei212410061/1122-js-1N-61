73eb22cfef090f0e3c92e92f288ae0d9a37f7c8e
haowei0218 <haoweizhou891@gmail.com>
2024-05-16 21:34:21 +0800

    W13-P4: Create company_61 and store_61 tables, and insert all data

--- demo/w13_61/supa_61.js
+++ demo/w13_61/supa_61.js
@@ -1,6 +1,8 @@
 //import {menu} from './data_61.js';
 //const url = "./api/data_61.json";
-import { _supabase } from "./client.supabase_61";
+const  SUPSBASE_ULR = 'https://itbleknccqovsbposbsv.supabase.co'
+const  SUPABASE_key = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml0Ymxla25jY3FvdnNicG9zYnN2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM5NTgyMTAsImV4cCI6MjAyOTUzNDIxMH0.tT_GJQ0EAzWNM5v-S8B5k5DA7vojt6l4WezlTOBXbnE'
+export const _supabase = supabase.createClient(SUPSBASE_ULR,SUPABASE_key);
 
 let menu = [];
 console.log('menu',menu);
