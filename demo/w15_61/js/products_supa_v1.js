89cb54ae2878ca40a6071b8067cdc45cd11e6533
212410061 <212410061@o365.tku.edu.tw>
2024-06-13 19:14:36 +0800

    vercel

--- demo/w15_61/js/products_supa_v1.js
+++ demo/w15_61/js/products_supa_v1.js
@@ -1,9 +1,10 @@
-
 import { _supabase } from './client.supabase_61.js';
-let product_61 = []
+let product_61 = [];
 const getProductsSupabase_61 = async () => {
   try {
-    let { data, error } = await _supabase.from('product_61').select('*,company_61(*)');
+    let { data, error } = await _supabase
+      .from('product_61')
+      .select('*,company_61(*)');
     return data; // return the fetched data, not product_61
   } catch (error) {
     console.log(error);
@@ -15,9 +16,10 @@ const productContainer = document.querySelector('.products-container');
 const companyBtns = document.querySelectorAll('.company-btn');
 
 const DisplayProducts = (products) => {
-  let productsContent = products.map((product) => {
-    const { title, price, localImg } = product;
-    return `
+  let productsContent = products
+    .map((product) => {
+      const { title, price, localImg } = product;
+      return `
       <div class="single-product">
         <img
           src="${localImg}"
@@ -30,27 +32,27 @@ const DisplayProducts = (products) => {
         </footer> 
       </div>
     `;
-  }).join('');
+    })
+    .join('');
   productContainer.innerHTML = productsContent;
 };
 
-companyBtns.forEach((btn)=>{
-  btn.addEventListener('click',async(e)=>{
+companyBtns.forEach((btn) => {
+  btn.addEventListener('click', async (e) => {
     const companyName = e.currentTarget.dataset.id;
     const products = await getProductsSupabase_61();
     console.log(companyName);
-    if(companyName === 'all'){
+    if (companyName === 'all') {
       product_61 = products;
-      
-    }else{
-      product_61 = products.filter((product)=>
-        product.company_61.name === companyName      
-      )
-      console.log(product_61)
+    } else {
+      product_61 = products.filter(
+        (product) => product.company_61.name === companyName
+      );
+      console.log(product_61);
       DisplayProducts(product_61);
     }
-  })
-})
+  });
+});
 
 document.addEventListener('DOMContentLoaded', async () => {
   const products_61 = await getProductsSupabase_61(); // assign the fetched data to a variable
